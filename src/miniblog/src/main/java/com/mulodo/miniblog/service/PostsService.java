b2b5d4278b85e1932cb2dd30abe3a6062f359642
Quang Vo <vo.quang@mulodo.com>
2015-02-12 16:19:49 +0700

    add Posts

---
+++ src/miniblog/src/main/java/com/mulodo/miniblog/service/PostsService.java
@@ -0,0 +1,16 @@
+package com.mulodo.miniblog.service;
+
+import java.util.List;
+
+import com.mulodo.miniblog.model.Posts;
+
+public interface PostsService 
+{
+	public boolean createPost(Posts post);
+	public Posts GetPostById(int id);
+	public boolean UpdatePost(Posts post);
+	public boolean DeletePost(int id);
+	public List<Posts> getAllPosts();
+	public List<Posts> getPostsForUser(int user_id);
+	
+}
